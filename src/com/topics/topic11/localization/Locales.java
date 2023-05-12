86864721020b6b9471c4704cd6ef67f274b94642
Bruno Henrique <brunoh915@gmail.com>
2023-05-11 22:40:25 -0300

    Anotando Locales e ResourceBundles.

--- src/com/topics/topic11/localization/Locales.java
+++ src/com/topics/topic11/localization/Locales.java
@@ -1,13 +1,38 @@
 package com.topics.topic11.localization;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class Locales {
 
+	// Referência:
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Lista de languages mantida por IANA: https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+		
+		// Formas de construir um Locale:
+		System.out.println("\n Formas de construir um Locale:");
+		
+		Locale loc = new Locale("PT");
+		loc = new Locale("PT", "BR");
+		loc = new Locale("PT", "BR", "BR");
+		
+		loc = Locale.JAPAN;
+		loc = Locale.JAPANESE;
+		
+		loc = Locale.forLanguageTag("pt-BR");
 		
 		// Locale e seu atributos:
+		System.out.println("\n Locale e seu atributos:");
+		
 		Locale locPT = new Locale("PT", "PT");
 		
 		System.out.println(locPT);
@@ -27,11 +52,85 @@ public class Locales {
 		
 		
 		// Default Locale:
+		System.out.println("\n Default Locale:");
+		
 		System.out.println(Locale.getDefault());
 		
 		Locale.setDefault(new Locale("en"));
 		
 		System.out.println(Locale.getDefault());
+		
+		
+		// Recuperando Locales instalados na JDK:
+		System.out.println("\n Recuperando Locales instalados na JDK:");
+		
+		Locale[] numberFormatLocales = NumberFormat.getAvailableLocales();
+		Locale[] dateFormatLocales = DateFormat.getAvailableLocales();
+		Locale[] locales = Locale.getAvailableLocales();
+		
+		// Formatando valores com Locales:
+		System.out.println("\n Formatando valores com Locales:");
+		
+		Locale frLocale = Locale.FRANCE;
+		Locale usLocale = Locale.US;
+		Locale ukLocale = Locale.UK;
+		
+		double number = 104565.4565d;
+		
+		NumberFormat frNumberFormat = NumberFormat.getInstance(frLocale);
+		NumberFormat usNumberFormat = NumberFormat.getInstance(usLocale);
+		NumberFormat ukNumberFormat = NumberFormat.getInstance(ukLocale);
+		
+		String frFormattedDouble = frNumberFormat.format(number);
+		String usFormattedDouble = usNumberFormat.format(number);
+		String ukFormattedDouble = ukNumberFormat.format(number);
+		
+		System.out.println(frFormattedDouble);
+		System.out.println(usFormattedDouble);
+		System.out.println(ukFormattedDouble);
+		
+		// Formatando LocalDate, LocalTime, DateTime, e ZonedDatetTime:
+		System.out.println("\n Formatando LocalDate, LocalTime, DateTime, e ZonedDatetTime:");
+		
+		LocalDateTime ldtm = LocalDateTime.now();
+		String pattern = "dd-MMMM-yyyy HH:mm:ss.SSS";
+		
+		DateTimeFormatter defaulDateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+		DateTimeFormatter localizedDeteTimeFormatter = DateTimeFormatter.ofPattern(pattern, Locale.JAPAN);
+		
+		String defaultFormat = defaulDateTimeFormatter.format(ldtm);
+		String localizedFormat = localizedDeteTimeFormatter.format(ldtm);
+		
+		System.out.println(defaultFormat);
+		System.out.println(localizedFormat);
+		
+		// Formatando LocalizedDateTime:
+		System.out.println("\n Formatando LocalizedDateTime:");
+		
+		LocalDateTime localizedLdtm = LocalDateTime.now();
+		ZoneId zoneIdSaoPaulo = TimeZone.getTimeZone("America/Sao_Paulo").toZoneId();
+		
+		DateTimeFormatter localizedTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+		String formattedLocalizedTime = localizedTimeFormatter.format(ZonedDateTime.of(localizedLdtm, zoneIdSaoPaulo));
+		System.out.println(formattedLocalizedTime);
+		
+		// Formatando DateFormat and SimpleDateFormatter
+		System.out.println("\n Formatando DateFormat and SimpleDateFormatter");
+		
+		Date date = new Date();
+		
+		// getDateTimeInstance || getDateInstance || getTimeInstance
+		DateFormat formatJpInstance = DateFormat.getDateTimeInstance(
+				DateFormat.FULL, DateFormat.FULL, Locale.JAPAN);
+		
+		DateFormat formatFrInstance = DateFormat.getDateTimeInstance(
+				DateFormat.FULL, DateFormat.FULL, Locale.FRANCE);
+		
+		String jpDateFormatted = formatJpInstance.format(date);
+		String frDateFormatted = formatFrInstance.format(date);
+		
+		System.out.println(jpDateFormatted);
+		System.out.println(frDateFormatted);
 	}
 
 }
--- src/com/topics/topic11/localization/ResourceBundles.java
+++ src/com/topics/topic11/localization/ResourceBundles.java
@@ -5,11 +5,46 @@ import java.util.ResourceBundle;
 
 public class ResourceBundles {
 
-	// Continuar: https://www.baeldung.com/java-resourcebundle
+	// Referência: https://www.baeldung.com/java-resourcebundle
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Locale currentLocale = Locale.getDefault();
-		ResourceBundle myResources = ResourceBundle.getBundle("MyResources", currentLocale);
+		//Locale currentLocale = Locale.getDefault();
+		//ResourceBundle myResources = ResourceBundle.getBundle("MyResources", currentLocale);
+		
+		// Pega arquivo com mensagens em Inglês
+		// Caso não encontre o arquivo, é lançado um java.util.MissingResourceException
+		System.out.println("\n Pega arquivo com mensagens em Inglês");
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle", Locale.US);
+		String greeting = bundle.getString("greeting");
+		System.out.println(greeting);
+		
+		// Muda locale default para PT-BR
+		Locale locPT_BR = new Locale("PT", "BR");
+		Locale.setDefault(locPT_BR);
+		
+		// Pega arquivo com mensagens em Português BR
+		System.out.println("\n Pega arquivo com mensagens em Português BR");
+		
+		bundle = ResourceBundle.getBundle("MessageBundle", Locale.getDefault());
+		greeting = bundle.getString("greeting");
+		System.out.println(greeting);
+		
+		// Muda locale default para PT de Portugal
+		Locale locPT = new Locale("PT");
+		Locale.setDefault(locPT);
+		
+		// Pega arquivo com mensagens em Português de Portugal
+		System.out.println("\n Pega arquivo com mensagens em Português de Portugal");
+		
+		bundle = ResourceBundle.getBundle("MessageBundle", Locale.getDefault());
+		greeting = bundle.getString("greeting");
+		System.out.println(greeting);
+		
+		System.out.println(bundle.getKeys().nextElement());
+		
+		// No Java 9 em diante, já não há problemas de encoding com arquivos bundle, tudo é lido em UTF-8
+		
 	}
 
 }
